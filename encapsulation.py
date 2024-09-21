d4246a5c70625b594997c11ae816ad57eb1d7dd5
Surya <surya.c@diggibyte.com>
2024-09-21 11:14:59 +0530

    Encapsulation in oops

--- encapsulation.py
+++ encapsulation.py
@@ -1,8 +1,58 @@
 
-class A:
-    def __sample(self):
-        print("Helo")
+# class company:
+#
+#     def __init__(self):
+#
+#         self.name="Digibyte"
+#
+#     def sample(self):
+#         print(self.name)
+#
+#
+# obj=company()
+#
+# obj.name="Amazon"
+#
+# obj.sample()
 
-obj=A()
 
-obj.__sample()
+#***************************** Private *********
+# class company:
+#
+#     def __init__(self):
+#
+#         self.__name="Digibyte"
+#
+#     def sample(self):
+#         print(self.__name)
+#
+#
+# obj=company()
+#
+# obj.__name="Amazon"
+#
+# obj.sample()
+
+
+#****************************** Produted ***************
+
+class company:
+
+    def __init__(self):
+
+        self._name="Digibyte"
+
+    def sample(self):
+        print(self._name)
+
+class office(company):
+
+    def demo(self):
+        print(self._name)
+
+
+obj=office()
+
+obj.name="Amazon"
+
+obj.demo()
