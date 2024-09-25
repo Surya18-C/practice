8dd1c7fc22295c0a284dc34b0648841229d907c0
Surya <surya.c@diggibyte.com>
2024-09-25 12:26:39 +0530

    Union function

---
+++ Sql/union.sql
@@ -0,0 +1,10 @@
+
+select * from employe 
+
+select * from dprt
+
+-- select * from employe UNION  select * from dprt
+
+-- select * from employe UNION ALl select * from dprt
+
+INSERT INTO dprt (dprt_id , dprt) VALUES (1,'Rex')
