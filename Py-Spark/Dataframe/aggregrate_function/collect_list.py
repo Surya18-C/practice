8e8b87c82bb39bacd917384366d9a93df6044c22
Surya <surya.c@diggibyte.com>
2024-10-15 14:11:34 +0530

    return all the elements in a element

---
+++ Py-Spark/Dataframe/aggregrate_function/collect_list.py
@@ -0,0 +1,11 @@
+from pyspark.shell import spark
+from pyspark.sql.functions import *
+
+data = [("Lap", 25), ("Computer", 30), ("Lap", 30),("Lap",15)]
+df = spark.createDataFrame(data, ["name", "age"])
+
+
+res=df.groupBy("name").agg(collect_list('age'))
+
+res.show()
+
